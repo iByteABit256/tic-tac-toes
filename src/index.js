54bc3fa72299efa80bc9a76e89b3db502b437680
sebe324 <sebeq05@gmail.com>
2024-03-30 18:00:26 +0100

    Fixed icon issue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import Favicon from "react-favicon";
 
 import App from "./app";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <Favicon url="./favicon.ico" />
     <App />
   </StrictMode>,
 );
